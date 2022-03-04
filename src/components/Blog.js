d6e960f390e3289921a74143eb6d200fec51340b
Martin Ovington <MartinOvington@msn.com>
2022-03-04 09:54:56 +0000

    ex5.7 toggle details on blogs

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,8 +1,35 @@
-import React from 'react'
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+const Blog = ({blog}) => {
+  const [viewDetails, setViewDetails] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+  
+  const toggleViewDetails = () => {
+    setViewDetails(!viewDetails)
+  }
+
+  return (
+    <div>
+      {viewDetails ?
+      <div style={blogStyle}>
+        {blog.title} {blog.author} 
+        <button onClick={toggleViewDetails}>hide</button><br></br>
+        {blog.url} <br></br>
+        likes {blog.likes}<button>like</button><br></br>  
+        {blog.user? blog.user.name : ''} 
+      </div> :
+      <div style={blogStyle}>
+        {blog.title} {blog.author}
+        <button onClick={toggleViewDetails}>view</button>
+      </div>
+    }
+    </div>
+)}
 
 export default Blog
